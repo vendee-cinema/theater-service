18af11904f28f0f513d7850fb62198319d5ecd8d
bagdenius <bagdensparrow@gmail.com>
2026-03-23 15:19:05 +0200

    feat: configure gRPC server

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { type MicroserviceOptions, Transport } from '@nestjs/microservices'
+import { PROTO_PATHS } from '@vendee-cinema/contracts'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(process.env.PORT ?? 3000)
+	const config = app.get(ConfigService)
+	const url = `${config.getOrThrow<string>('GRPC_HOST')}:${config.getOrThrow<number>('GRPC_PORT')}`
+	app.connectMicroservice<MicroserviceOptions>({
+		transport: Transport.GRPC,
+		options: {
+			package: ['theater.v1'],
+			protoPath: [PROTO_PATHS.THEATER],
+			url,
+			loader: {
+				keepCase: false,
+				longs: String,
+				enums: String,
+				defaults: true,
+				oneofs: true
+			}
+		}
+	})
+	await app.startAllMicroservices()
+	await app.init()
 }
 bootstrap()
